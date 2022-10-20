df49836b6c3a37ce89623423fec1b6e4391a2948
Minhyeong <alsuddl25@icloud.com>
2022-10-20 20:59:25 +0900

    오타 수정

--- services/posts.service.js
+++ services/posts.service.js
@@ -31,16 +31,15 @@ class PostService {
     // 저장소(Repository)에게 데이터를 요청합니다.
 
     const postsOne = await this.postRepository.findOnePost(postId);
-    return postsOne;
     // 비즈니스 로직을 수행한 후 사용자에게 보여줄 데이터를 가공합니다.
-    // return {
-    //   postId: postsOne.postId,
-    //   nickname: postsOne.nickname,
-    //   title: postsOne.title,
-    //   content: postsOne.content,
-    //   createdAt: postsOne.createdAt,
-    //   updatedAt: postsOne.updatedAt,
-    // };
+    return {
+      postId: postsOne.postId,
+      nickname: postsOne.nickname,
+      title: postsOne.title,
+      content: postsOne.content,
+      createdAt: postsOne.createdAt,
+      updatedAt: postsOne.updatedAt,
+    };
   };
 
   //게시글작성
