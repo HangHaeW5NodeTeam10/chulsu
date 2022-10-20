22d77b76eccca696c1321debc42457eee4761fb2
Minhyeong <alsuddl25@icloud.com>
2022-10-20 11:37:16 +0900

    코멘트 레파지토리/서비스 수정

--- services/comments.service.js
+++ services/comments.service.js
@@ -4,59 +4,72 @@ class CommentsService {
   commentsRepository = new CommentsRepository();
 
   getComments = async (postId) => {
-    // 저장소(Repository)에게 데이터를 요청합니다.
-    const allComments = await this.commentsRepository.getComments(postId);
+    try {
+      const existPost = await this.commentsRepository.findThePost(postId);
+      if (!existPost) throw new Error('존재하지 않는 포스트입니다. ');
 
-    /*
-    repository에서 잘못된 postId 일경우 에러를 리턴하는데 에러를 정렬하려고 하거나 map으로 가공하려고 하면 
-    또 에러가 발생할 것 같아서 에러가 있다면 그냥 에러를 리턴하도록 에러여부 필터링 함.
-    */
-    if (allComments.errorMessage) {
-      return { errorMessage: allComments.errorMessage };
+      const allComments = await this.commentsRepository.getComments(postId);
+      allComments
+        .sort((a, b) => {
+          return b.createdAt - a.createdAt;
+        })
+        .map((comment) => {
+          return {
+            commentId: comment.commentId,
+            postId: comment.postId,
+            nickname: comment.User.nickname,
+            comment: comment.comment,
+            createdAt: comment.createdAt,
+            updatedAt: comment.updatedAt,
+          };
+        });
+
+      return allComments;
+    } catch (error) {
+      console.error(error);
+      return { errorMessage: error.message };
     }
-
-    // 비즈니스 로직을 수행한 후 사용자에게 보여줄 데이터를 가공합니다.
-    allComments.sort((a, b) => {
-      return b.createdAt - a.createdAt;
-    });
-
-    allComments.map((comment) => {
-      return {
-        commentId: comment.commentId,
-        postId: comment.postId,
-        nickname: comment.User.nickname,
-        comment: comment.comment,
-        createdAt: comment.createdAt,
-        updatedAt: comment.updatedAt,
-      };
-    });
-
-    return allComments;
   };
 
   createComment = async (postId, userId, comment) => {
-    // 저장소(Repository)에게 데이터를 요청합니다.
-    const newComment = await this.commentsRepository.createComment(postId, userId, comment);
+    try {
+      const existPost = await this.commentsRepository.findThePost(postId);
+      if (!existPost) throw new Error('존재하지 않는 포스트입니다. ');
 
-    if (newComment.errorMessage) {
-      return { errorMessage: newComment.errorMessage };
+      await this.commentsRepository.createComment(postId, userId, comment);
+
+      return { message: '댓글이 생성되었습니다.' };
+    } catch (error) {
+      console.error(error);
+      return { errorMessage: error.message };
     }
-
-    return newComment;
   };
 
   updateComment = async (commentId, userId, comment) => {
-    // 저장소(Repository)에게 데이터를 요청합니다.
-    const updateResult = await this.commentsRepository.updateComment(commentId, userId, comment);
+    try {
+      const existComment = await this.commentsRepository.findTheComment(commentId);
+      if (!existComment) throw new Error('존재하지 않는 댓글입니다. ');
+      if (existComment.userId !== userId) throw new Error('댓글 작성자만 댓글을 수정할 수 있습니다.');
 
-    return updateResult;
+      await this.commentsRepository.updateComment(commentId, comment);
+
+      return { message: '댓글이 수정되었습니다.' };
+    } catch (error) {}
   };
 
   deleteComment = async (commentId, userId) => {
-    // 저장소(Repository)에게 데이터를 요청합니다.
-    const deleteResult = await this.commentsRepository.deleteComment(commentId, userId);
+    try {
+      const existComment = await this.commentsRepository.findTheComment(commentId);
+      if (!existComment) throw new Error('존재하지 않는 댓글입니다. ');
+      if (existComment.userId !== userId) throw new Error('댓글 작성자만 댓글을 삭제할 수 있습니다.');
 
-    return deleteResult;
+      await this.commentsRepository.deleteComment(commentId);
+
+      return deleteResult;
+    } catch (error) {
+      console.error(error);
+      return { errorMessage: error.message };
+    }
   };
 }
 
